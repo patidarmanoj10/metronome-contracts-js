d4d87f19129c0c79cb3a7b6ff44873d08eb04e99
Gabriel Montes <gabriel@bloq.com>
2018-05-03 18:01:37 -0300

    Update to Ropsten contract addresses

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const metToken = require('./contracts/METToken')
 const auctions = require('./contracts/Auctions')
 const autonomousConverter = require('./contracts/AutonomousConverter')
 
-const metTokenAddress = '0x8f773ca2366f6d07e3bc06d597324c4ec754fec7'
-const auctionsAddress = '0x26f6bea75bc87f6216aeef9c8f9a8621c64e348c'
-const autonomousConverterAddress = '0x36d95f7e25498bccb6c9d2eac47896a64ba7d015'
+const metTokenAddress = '0xe0df19e55ebbbe076c3e393ff5d0418a2a0de5f8'
+const auctionsAddress = '0x7e25f34e0f16059bea3ffae7d6545c04896cced2'
+const autonomousConverterAddress = '0x6d0cb3142e66f5f3cf196b1df32082bf5f3325d3'
 
 class Metronome {
   constructor (web3) {
